b047a1a24d163751b31e68a5da6408d012bfc8d4
Analin Flores <foglzerika@gmail.com>
2020-02-04 21:36:00 -0600

    fix/configuration email

---
+++ templates/emailNotification.js
@@ -0,0 +1,38 @@
+module.exports = {
+    templateExample: (message) => { return `
+    <mjml>
+  <mj-body background-color="#d6dde5">
+
+    <mj-section background-color="#ffffff" padding-bottom="20px" padding-top="20">
+      <mj-column width="100%" vertical-align="top">
+        <mj-image href="/" src="http://191n.mj.am/img/191n/3s/xxr.png" alt="Amario logo" align="center" border="none" width="182px" padding-left="0px" padding-right="0px" padding-bottom="0px" padding-top="0"></mj-image>
+      </mj-column>
+    </mj-section>
+    <mj-section background-color="#ffffff" background-url="http://191n.mj.am/img/191n/3s/xm6.jpg" padding-bottom="27px" padding-top="0">
+      <mj-column width="100%" vertical-align="top">
+        <mj-text align="center" color="#000000" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="13px" padding-left="25px" padding-right="25px" padding-bottom="0px" padding-top="0">
+          <p><span style="font-weight: bold;"><span style="color: rgb(255, 255, 255);"><span style="font-size: 16px;"><br></span></span>
+            </span>
+          </p>
+          <p><span style="font-size: 27px;"><a href="https://mjml.io" style="text-decoration: none; color: inherit;"><span style="font-weight: bold;"><span style="color: rgb(255, 255, 255);">Felicidades</span></span>
+            </a>
+            </span>
+          </p>
+          <p><span style="font-weight: bold;"><span style="color: rgb(255, 255, 255);"><span style="font-size: 27px;"> Hola !! </span></span>
+            </span>
+          </p>
+          <p><span style="font-weight: bold;"><span style="color: rgb(255, 255, 255);"><span style="font-size: 27px;"> Estás a un paso de adopar con </span></span>
+            </span>
+          </p>
+          <p><span style="font-weight: bold;"><span style="color: rgb(255, 255, 255);"><span style="font-size: 27px;">IronPets</span></span>
+            </span>
+          </p>
+        </mj-text>
+        <mj-button background-color="#414141" color="#FFFFFF" font-size="15px" align="center" vertical-align="middle" border="none" padding="15px 30px" border-radius="3px" href="https://mjml.io" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif"
+          padding-left="25px" padding-right="25px" padding-bottom="10px" padding-top="70"> Salva a una mascota !</mj-button>
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>
+    PASTE THE HTML CODE HERE AND USE THE ${message} VARIABLE WHERE YOU WANT
+    ` }
